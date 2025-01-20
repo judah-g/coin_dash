9baac8aafae63bc5d37f01561469483fcdf41376
339633265 <339633265@2260I-L29-001>
2025-01-20 09:20:16 -0500

    unfinished

--- hud.gd
+++ hud.gd
@@ -12,6 +12,26 @@ func update_score(value):
 func update_timer(value):
 	$MarginContainer/Time.text = str(value)
 
+func show_message(text):
+	$Message.text = text
+	$Message.show()
+	$Timer.start()
+
+func _on_timer_timeout():
+	$Message.hide
+
+func _on_start_button_pressed():
+	$StartButton.hide()
+	$Message.hide()
+	start_game.emit()
+
+func show_game_over():
+	show_message("Game Over")
+	await $Timer.timeout
+	$StartButton.show()
+	$Message.text = "Coin Dash!"
+	$Message.show()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
--- main.gd
+++ main.gd
@@ -25,6 +25,8 @@ func new_game():
 	$Player.show()
 	$GameTimer.start()
 	spawn_coins()
+	$HUD.update_score(score)
+	$HUD.update_timer(time_left)
 
 func spawn_coins():
 	for i in level + 4:
@@ -32,6 +34,29 @@ func spawn_coins():
 		add_child(c)
 		c.position = Vector2(randi_range(0, screensize.x), 
 				randi_range(0, screensize.y))
+
+func _on_game_timer_timeout():
+	time_left -= 1
+	$HUD.update_timer(time_left)
+	if time_left <= 0:
+		game_over()
+
+func _on_player_hurt():
+	game_over()
+
+func _on_player_pickup():
+	score += 1
+	$HUD.update_score(score)
+
+func game_over():
+	playing = false
+	$GameTimer.stop()
+	get_tree().call_group("coins", "queue_free")
+	$HUD.show_game_over()
+	$Player.die()
+
+func _on_hud_start_game():
+	new_game()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
